38a95787d791487f855f51f2049286984a94decb
Борзунов Андрей <andry73@yandex.ru>
2025-06-10 13:20:36 +0500

    initial

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+if __name__ == "__main__":
+
+    print(get_mask_card_number(7000792289606361))
+    print(get_mask_account(7365410840135874305))
---
+++ src/masks.py
@@ -0,0 +1,25 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Маскировка номера банковской карты"""
+    card_number_str = str(card_number)
+
+    mask_card_number = ""
+    index = 0
+    for item in card_number_str:
+        if index % 4 == 0 and index > 0:
+            mask_card_number += " "
+        if 5 < index < 12:
+            mask_card_number += "*"
+        else:
+            mask_card_number += item
+        index += 1
+
+    return mask_card_number
+
+
+def get_mask_account(account_number: int) -> str:
+    """Маскировка номера банковского счета"""
+    account_number_str = str(account_number)
+
+    mask_account_number = "**" + account_number_str[-4:]
+
+    return mask_account_number
