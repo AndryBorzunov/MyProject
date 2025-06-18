82a06b9c03a04138e951a655d22dbb9640f5794c
Борзунов Андрей <andry73@yandex.ru>
2025-06-18 19:37:25 +0500

    sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,11 @@ def filter_by_state(list_of_dicts: List, state='EXECUTED') -> List:
             filter_list.append(dict_item)
 
     return filter_list
+
+
+def sort_by_date(list_of_dicts: List, reverse: bool=True) -> List:
+    """ Сортировка полученного списка по дате
+        по умолчанию - сортировка по убыванию
+    """
+
+    return sorted(list_of_dicts, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=reverse)
