1ee1491c234fd6b4b1afaf11c401ed8ff2d2922f
Борзунов Андрей <andry73@yandex.ru>
2025-06-18 18:11:13 +0500

    filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 from datetime import datetime
 
 def filter_by_state(list_of_dicts: List, state='EXECUTED') -> List:
+    """ Выборка словарей из списка словарей с заданным статусом"""
     filter_list = []
     for dict_item in list_of_dicts:
         if dict_item['state'] == state:
