7ae22e11d5baf34badfb3bf121a0f2af4dc3c564
Борзунов Андрей <andry73@yandex.ru>
2025-06-18 18:10:00 +0500

    filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,10 @@
+from typing import List, Dict
+from datetime import datetime
+
+def filter_by_state(list_of_dicts: List, state='EXECUTED') -> List:
+    filter_list = []
+    for dict_item in list_of_dicts:
+        if dict_item['state'] == state:
+            filter_list.append(dict_item)
+
+    return filter_list
