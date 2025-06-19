0d9e9416dd5b4b8f3b422c108c3a6aa04790b7f3
Борзунов Андрей <andry73@yandex.ru>
2025-06-19 07:37:31 +0500

    relize Homework 10.1

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,20 @@
-from typing import List, Dict
 from datetime import datetime
+from typing import List
 
-def filter_by_state(list_of_dicts: List, state='EXECUTED') -> List:
-    """ Выборка словарей из списка словарей с заданным статусом"""
+
+def filter_by_state(list_of_dicts: List, state: str = "EXECUTED") -> List:
+    """Выборка словарей из списка словарей с заданным статусом"""
     filter_list = []
     for dict_item in list_of_dicts:
-        if dict_item['state'] == state:
+        if dict_item["state"] == state:
             filter_list.append(dict_item)
 
     return filter_list
 
 
-def sort_by_date(list_of_dicts: List, reverse: bool=True) -> List:
-    """ Сортировка полученного списка по дате
-        по умолчанию - сортировка по убыванию
+def sort_by_date(list_of_dicts: List, reverse: bool = True) -> List:
+    """Сортировка полученного списка по дате
+    по умолчанию - сортировка по убыванию
     """
 
-    return sorted(list_of_dicts, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=reverse)
+    return sorted(list_of_dicts, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=reverse)
