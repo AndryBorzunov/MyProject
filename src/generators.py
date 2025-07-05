3e13ae841df136b7eb854c89d5dfb2600e1bc699
Борзунов Андрей <andry73@yandex.ru>
2025-07-05 22:31:55 +0500

    test_complite

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,9 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Generator
 
 
-def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> iter:
-    """
-    Генератор транзакций, отфильтрованных по валюте операции
-    """
+def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> Generator[Any | None, Any, None]:
+    """Генератор транзакций, отфильтрованных по валюте операции"""
+
     if len(transactions) == 0:
         raise ValueError("Нет данных")
 
@@ -21,15 +20,12 @@ def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> ite
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
-    return []
-    #raise StopIteration("Транзакции в заданной валюте отсутствуют")
 
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Generator[Any | None, Any, None]:
+    """Принимает список транзакций и возвращает описание каждой транзакции по очереди"""
 
-def transaction_descriptions(transactions) -> iter:
-    """
-    Принимает список транзакций и возвращает описание каждой транзакции
-    по очереди
-    """
+    if len(transactions) == 0:
+        raise ValueError("Нет данных")
 
     for transaction in transactions:
         if "description" not in transaction:
@@ -38,7 +34,7 @@ def transaction_descriptions(transactions) -> iter:
         yield transaction["description"]
 
 
-def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> iter:
+def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> Generator[Any | None, Any, None]:
     """
     Генератор может сгенерировать номера карт в заданном диапазоне номеров
     """
@@ -48,6 +44,5 @@ def card_number_generator(start: int = 1, stop: int = 9999999999999999) -> iter:
         if len_num <= 16:
             len_zero = 16 - len_num
             card_number = "0" * len_zero + str(gen_num)
-            card_number = card_number[:4] + " " + card_number[4:8] + " " + \
-                          card_number[8:12] + " " + card_number[-4:]
+            card_number = card_number[:4] + " " + card_number[4:8] + " " + card_number[8:12] + " " + card_number[-4:]
             yield card_number
